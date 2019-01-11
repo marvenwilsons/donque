dad134c33870356308aac8430919222286f4d874
Marven Wilson Donque <marvenwilsons@gmail.com>
2019-01-11 11:53:33 -0500

    statuc-validate-string

--- server/sys/core-apps/app/static-validate.js
+++ server/sys/core-apps/app/static-validate.js
@@ -8,20 +8,67 @@ class validate {
             }else{
                 this.final.push(false)
             }
+
+            return this
         }
-        this.isfalse = (value) => {
+        this.isFalse = (value) => {
             if (!value) {
                 this.final.push(true)
             } else {
                 this.final.push(false)
             }
+
+            return this
+        }
+        
+        this.minChar = (number) => {
+            this.data.length > number ? this.final.push(true) : this.final.push(`should have ${number} minimum characters`)
+            return this
+        }
+        this.maxChar = (number) => {
+            this.data.length > number ? this.final.push(true) : this.final.push(`should only have ${number} maximum characters`)
+            return this
         }
         this.hasCapitalLetters = () => {
-            this.final.push(true)
+            const regex = /.*[A-Z]/g;
+            regex.exec(this.data) != null ? this.final.push(true) : this.final.push('should have capital letters')            
+            return this
+        }
+        this.hasWhiteSpace = () => {
+            const regex = /\s/gim
+            regex.exec(this.data) !== null ? this.final.push(true) : this.final.push('should have white space')
+            return this
+        }
+        this.hasSpecialCharacters = () => {
+            const regex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/gim;
+            regex.exec(this.data) != null ? this.final.push(true) : this.final.push('should have special characters')
+            return this
+        }
+        this.required = () => {
+            if(this.data){
+                this.final.push(true)
+            }else{
+                this.final.push('required')
+            }
+            return this
+        }
+        this.hasNumbers = () => {
+            const regex = /.*[0-9]/g;
+            regex.exec(this.data) != null ? this.final.push(true) : this.final.push('should have numbers')
+            return this
+        }
+        this.hasSmallLetters = () => {
+            const regex = /.*[a-z]/g;
+            regex.exec(this.data) != null ? this.final.push(true) : this.final.push('should have small letters')
             return this
         }
         this.done = () => {
-            return this.final.every(items => items == true)
+            if (this.final.every(items => items == true)){
+                return true
+            }else {
+                return this.final.filter(e => e != true)
+            }
+            return this.final
         }
     }
 }
---
+++ server/sys/core-apps/app/temp.js
@@ -0,0 +1,108 @@
+const appAgent = require('./app-agent')
+
+const data = 'az E2c#$'
+
+const x =
+    appAgent
+        .staticMethods('mass-validate', data)
+        .hasCapitalLetters()
+        .hasSmallLetters()
+        .hasWhiteSpace()
+        .hasSpecialCharacters()
+        .isTrue(true)
+        .isFalse(false)
+        .minChar(1)
+        .maxChar(4)
+        .required()
+        .hasNumbers()
+        .done()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(`
+
+
+`)
+console.log(x)
+console.log(`
+
+
+`)
