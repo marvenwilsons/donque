b110508a14fe617a0eb98f730b859bc9075ab0c3
Marven Wilson Donque <marvenwilsons@gmail.com>
2019-03-11 16:13:27 -0400

    added label field for clear error response support

--- server/sys/core-apps/app/sys/cmd_lib/utils/static-validate.js
+++ server/sys/core-apps/app/sys/cmd_lib/utils/static-validate.js
@@ -1,6 +1,7 @@
 class validate {
-    constructor(data) {
+    constructor(data,label) {
         this.data = data
+        this.label = label
         this.final = []
         this.isTrue = (value,err) => {
             if (value) {
@@ -21,74 +22,81 @@ class validate {
             return this
         }
 
-        this.minChar = (number,label) => {
-            this.data.length > number ? this.final.push(true) : this.final.push(`${label} should have ${number} minimum characters`)
+        this.minChar = (number) => {
+            this.data.length > number ? this.final.push(true) : this.final.push(`${this.label} should have ${number} minimum characters`)
             return this
         }
-        this.maxChar = (number, label) => {
-            this.data.length > number ? this.final.push(true) : this.final.push(`${label} should only have ${number} maximum characters`)
+        this.maxChar = (number) => {
+            this.data.length > number ? this.final.push(true) : this.final.push(`${this.label} should only have ${number} maximum characters`)
             return this
         }
-        this.hasCapitalLetters = (condition, label) => {
+        this.hasCapitalLetters = (condition) => {
             const regex = /.*[A-Z]/g;
             if (condition == undefined || condition == true || condition == null) {
-                regex.exec(this.data) != null ? this.final.push(true) : this.final.push(`${label} should have capital letters`)
+                regex.exec(this.data) != null ? this.final.push(true) : this.final.push(`${this.label} should have capital letters`)
             } else {
-                regex.exec(this.data) != null ? this.final.push(`${label} should not have capital letters`) : this.final.push(true)
+                regex.exec(this.data) != null ? this.final.push(`${this.label} should not have capital letters`) : this.final.push(true)
             }
 
             return this
         }
-        this.hasWhiteSpace = (condition, label) => {
+        this.hasWhiteSpace = (condition) => {
             const regex = /\s/gim
             if (condition == undefined || condition == true || condition == null) {
-                regex.exec(this.data) !== null ? this.final.push(true) : this.final.push(`${label} should have white space`)
+                regex.exec(this.data) !== null ? this.final.push(true) : this.final.push(`${this.label} should have white spaces`)
             } else {
-                regex.exec(this.data) !== null ? this.final.push(`${label} should not have white space`) : this.final.push(true)
+                regex.exec(this.data) !== null ? this.final.push(`${this.label} should not have white spaces`) : this.final.push(true)
             }
             return this
         }
-        this.hasSpecialCharacters = (condition, label) => {
+        this.hasSpecialCharacters = (condition) => {
             const regex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/gim;
             if (condition == undefined || condition == true || condition == null) {
-                regex.exec(this.data) != null ? this.final.push(true) : this.final.push(`${label} should have special characters`)
+                regex.exec(this.data) != null ? this.final.push(true) : this.final.push(`${this.label} should have special characters`)
             } else {
-                regex.exec(this.data) != null ? this.final.push(`${label} should not have special characters`) : this.final.push(true)
+                regex.exec(this.data) != null ? this.final.push(`${this.label} should not have special characters`) : this.final.push(true)
             }
             return this
         }
-        this.required = (label) => {
+        this.required = () => {
             if (this.data) {
                 this.final.push(true)
             } else {
-                this.final.push(`${label} is required`)
+                this.final.push(`${this.label} is required`)
             }
             return this
         }
-        this.hasNumbers = (condition, label) => {
+        this.hasNumbers = (condition) => {
             const regex = /.*[0-9]/g;
             if (condition == undefined || condition == true || condition == null) {
-                regex.exec(this.data) != null ? this.final.push(true) : this.final.push(`${label} should have numbers`)
+                regex.exec(this.data) != null ? this.final.push(true) : this.final.push(`${this.label} should have numbers`)
             } else {
-                regex.exec(this.data) != null ? this.final.push(`${label} should not have numbers`) : this.final.push(true)
+                regex.exec(this.data) != null ? this.final.push(`${this.label} should not have numbers`) : this.final.push(true)
             }
             return this
         }
-        this.hasSmallLetters = (condition, label) => {
+        this.hasSmallLetters = (condition) => {
             const regex = /.*[a-z]/g;
             if (condition == undefined || condition == true || condition == null) {
-                regex.exec(this.data) != null ? this.final.push(true) : this.final.push(`${label} should have small letters`)
+                regex.exec(this.data) != null ? this.final.push(true) : this.final.push(`${this.label} should have small letters`)
             } else {
-                regex.exec(this.data) != null ? this.final.push(`${label} should not have small letters`) : this.final.push(true)
+                regex.exec(this.data) != null ? this.final.push(`${this.label} should not have small letters`) : this.final.push(true)
             }
 
             return this
         }
         this.done = () => {
             if (this.final.every(items => items == true)) {
-                return true
+                return {
+                    hasError: false
+                }
             } else {
-                return this.final.filter(e => e != true)
+                // return this.final.filter(e => e != true)
+                return {
+                    hasError: true,
+                    error:this.final.filter(e => e != true)[0],
+                    errors:this.final.filter(e => e != true)
+                }
             }
         }
     }
