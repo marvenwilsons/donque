7af4bfdb27689fbf7acb8d8da0bdec889feba421
Marven Wilson Donque <marvenwilsons@gmail.com>
2019-03-12 10:18:21 -0400

    patch work

--- server/sys/core-apps/app/sys/cmd_lib/utils/utils.js
+++ server/sys/core-apps/app/sys/cmd_lib/utils/utils.js
@@ -1,7 +1,9 @@
 const validator = require('./static-validate')
+const security = require('./encrypt')
 const encrypt = require('./encrypt')
 
 module.exports = {
+    security,
     encrypt,
     validator
 }
