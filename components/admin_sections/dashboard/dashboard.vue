1190a684dd98e190b93ff70ea6f250191b901ad1
Marven Wilson Donque <marvenwilsons@gmail.com>
2019-06-16 10:42:00 -0400

    edit dashboard scafold

--- components/admin_sections/dashboard/dashboard.vue
+++ components/admin_sections/dashboard/dashboard.vue
@@ -1,21 +1,22 @@
 <template>
-    <div>
-        Analytics here
-        {{this.$store.state.user}}
+  <div class="fullheight-percent">
+    <div class="borderred fullheight-percent">
+        Dashboard widgets here!
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    beforeCreate(){
-        this.$store.commit('pane_system/set_pane_config', {
-            head_visibility: false,
-            title: 'Dashboard',
-        })
-    }
-}
+  beforeCreate() {
+    this.$store.commit("pane_system/set_pane_config", {
+      head_visibility: false,
+      title: "Dashboard",
+      pane_Width:"100%"
+    });
+  }
+};
 </script>
 
 <style>
-
 </style>
