8eec612a0be262097cfbe77b4879c6ae53b4b1b0
Marven Wilson Donque <marvenwilsons@gmail.com>
2019-06-22 20:42:47 -0400

    added pages pane

---
+++ components/admin_sections/pages/page-selected/page-selected.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div>details here</div>
+    {{data}}
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  props: ["data", "my_pane_index"],
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters({
+      data_list: "pane_system/data_list"
+    })
+  },
+  mounted() {
+    this.$store.commit("pane_system/alter_pane_config", {
+      pane_index: this.my_pane_index,
+      alter: {
+        title: `${this.data.name} page`
+      }
+    });
+  },
+  beforeCreate() {
+    this.$store.commit("pane_system/set_pane_config", {
+      title: " ",
+      pane_width: "950px",
+      renderOnce: true,
+    });
+  }
+};
+</script>
+
+<style>
+</style>
--- components/admin_sections/pages/page.vue
+++ components/admin_sections/pages/page.vue
@@ -1,20 +1,84 @@
 <template>
-    <div>
-        page here
+  <div class="flex1">
+    <div class="fullheight-percent">
+      <div class="pad125">
+        <div class="flex">
+          <div class="flex1 padright025">search:</div>
+          <div class="flex7">
+            <input class="flex fullwidth" type="text">
+          </div>
+          <div class="flex1">
+            <i class="fas fa-search padleft025 pointer"></i>
+            <i class="fas fa-plus pointer"></i>
+          </div>
+        </div>
+      </div>
+      <div class="fullheight-percent relative" style="overflow:auto;">
+        <div class="absolute fullwidth fullheight-percent">
+          <div
+            class="pointer margin050 pad050 flex"
+            v-for="(page,p_index) in pages"
+            :key="`dq-page-list-${p_index}-${page.name}`"
+            :id="`dq-page-${p_index}`"
+            :style="{background: active === `dq-page-${p_index}` || cur_actv == `dq-page-${p_index}` ? hoverBgColor : ''}"
+            @mouseover="active = `dq-page-${p_index}`"
+            @mouseleave="cur_actv != `dq-page-${p_index}` && (active = undefined)"
+            @click="cur_actv = `dq-page-${p_index}`,$store.dispatch('pane_system/open',{name: 'pageSelected', index: my_pane_index, data: page, data_index: p_index})"
+          >
+            <div class="flex1">
+              <div>
+                <strong>{{page.name}}</strong>
+              </div>
+              <div>last-updated: MM/DD/YY</div>
+            </div>
+            <div class="flex flex1 flexend" style="align-items: center;">
+              <!-- <i
+                class="pad050 dq-page-item fas fa-edit"
+                :id="`dq-page-i-${p_index}`"
+                :style="{background: i_active === `dq-page-i-${p_index}` && cur_actv === `dq-page-${p_index}` ? heverBgColor2 : ''}"
+                @mouseover="i_active = `dq-page-i-${p_index}`"
+              ></i>
+              <i
+                class="pad050 dq-page-item fas fa-redo-alt"
+                :id="`dq-page-ref-${p_index}`"
+                :style="{background: i_active === `dq-page-ref-${p_index}` && cur_actv === `dq-page-${p_index}` ? heverBgColor2 : ''}"
+                @mouseover="i_active = `dq-page-ref-${p_index}`"
+              ></i> -->
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    beforeCreate() {
-        this.$store.commit('pane_system/set_pane_config', {
-            title: 'Page list',
-            pane_width: '350px'
-        })
-    }
-}
+  props: ["my_pane_index"],
+  data() {
+    return {
+      cur_actv: undefined,
+      i_active: undefined,
+      active: undefined,
+      hoverBgColor: this.$store.state.theme.notify_tile_body_bg_hover_color,
+      heverBgColor2: this.$store.state.theme.heading_bg_color,
+      pages: [
+        { name: "home", lastModified: "" },
+        { name: "products", lastModified: "" }
+      ]
+    };
+  },
+  beforeCreate() {
+    this.$store.commit("pane_system/set_pane_config", {
+      title: "Page list",
+      pane_width: "350px"
+    });
+  }
+};
 </script>
 
 <style>
-
+.dq-page-item {
+  border-radius: 100%;
+}
 </style>
