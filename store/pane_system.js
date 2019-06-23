71fa99a997b7c9300bf5dc1f0b503eba8599795e
Marven Wilson Donque <marvenwilsons@gmail.com>
2019-06-22 20:42:24 -0400

    added renderOnce system to pane_system

--- store/pane_system.js
+++ store/pane_system.js
@@ -3,6 +3,8 @@ export const state = () => ({
     pane_index_config_list: [],
     pane_got_close: false,
     pane_index_list: [],
+    pane_data_obj: {},
+    pane_data_index: undefined,
     root: undefined
 })
 
@@ -18,7 +20,8 @@ export const getters = {
         // }
         return state.pane_index_list
     },
-    close_state: state => state.pane_got_close
+    close_state: state => state.pane_got_close,
+    data_list: state => state.pane_data_list
 }
 
 
@@ -31,14 +34,20 @@ export const mutations = {
         if (state.pane_index_list[0] != payload){
             state.pane_index_config_list = []
             state.pane_index_list = [`${payload}`]
+            state.pane_data_list = []
             state.root = payload
         }
     },
     /**
      * pane push adds one new pane to the view
      */
-    pane_push(state,payload) {
-        state.pane_index_list.push(payload)
+    pane_push(state,{compName,data}) {
+        // console.log('pane_push')
+        state.pane_index_list.push(compName)
+        
+        if (data != undefined){
+            state.pane_data_obj[compName] = data
+        }
     },
     /**
      * pane reset from will execute when a "non last index" pane
@@ -46,17 +55,51 @@ export const mutations = {
      * it deducts pane starting from the last index to the origin
      * of the pane_push event
      */
-    pane_replace_from(state,{startingIndex, compName, numOfComps}) {
+    pane_replace_from(state,{startingIndex, compName, numOfComps, data, data_index}) {
         /**
          * to avoid re redering the same component twice
          * when the user hit the same option twice
          */
         if (state.pane_index_list[startingIndex] != compName){
+
             // remove
             state.pane_index_list.splice(numOfComps, startingIndex)
             state.pane_index_config_list.splice(numOfComps, startingIndex)
+
             // replace
             state.pane_index_list.splice(numOfComps, 0, compName)
+            
+        } else if (state.pane_index_config_list[startingIndex].renderOnce){
+            /**
+             * Occurs when a list is using one component and re used but the data is dynamically
+             * different everytime.
+             */
+
+            // the index number of the item from the array origin
+            state.data_index = data_index
+
+            // by removing
+            state.pane_index_list.splice(numOfComps, startingIndex)
+            state.pane_index_config_list.splice(numOfComps, startingIndex)
+            state.pane_data_list = []
+
+            // replacing to trigger re render
+            // for some reason vue or maybe js wont trigger "re-render" even after
+            // the component removal, my guess is because the removal code and the
+            // insertion code executes at the same time, so I wrap it in a setTimeout func
+            // so that the removal will execute first then the insertion will execute next
+            // and my guess is write lol.
+            setTimeout(() => {
+                /**
+                 * why do I want to re render the same component if I can just pass the data in that component as a prop?
+                 * because I want to execute the "vue life cycle hooks" everytime the data changes, thats why!
+                 */
+                this.commit('pane_system/pane_push', {
+                    compName: compName,
+                    data,
+                    data_index
+                })
+            }, 0);
         }
     },
     /**
@@ -89,7 +132,8 @@ export const mutations = {
             pane_width,
             closable,
             maximizable,
-            head_visibility
+            head_visibility,
+            renderOnce
         } = payload
 
         /**
@@ -106,6 +150,8 @@ export const mutations = {
         typeof head_visibility === 'boolean' ? final_conf.head_visibility = payload.head_visibility : final_conf.head_visibility = true
         // title
         title ? final_conf.title = payload.title : final_conf.title = 'untitled pane'
+        // render once
+        typeof renderOnce === 'boolean' ? final_conf.renderOnce = payload.renderOnce : final_conf.renderOnce = false
 
 
         /**
@@ -124,6 +170,13 @@ export const mutations = {
          * Push to state
          */
         state.pane_index_config_list.push(final_conf)
+    },
+    alter_pane_config(state, {pane_index, alter}) {        
+        const keys = Object.keys(alter)
+
+        keys.map(key => {
+            state.pane_index_config_list[pane_index][key] = alter[key]
+        })
     }
 }
 
@@ -133,7 +186,7 @@ export const actions = {
         commit('pane_reset', context)
     },
     // only executes on non zero index items
-    open({ commit, state }, {index,name}) {
+    open({ commit, state }, {index,name,data, data_index}) {
         /**
          * pane dependencies
          */
@@ -157,20 +210,25 @@ export const actions = {
          * at this point user can only do pane_reset and pane_push
          */
         if(n_comps_mw === 1){
-            commit('pane_push', component_name)
+            commit('pane_push', {
+                compName: component_name, 
+                data
+            })
         }
 
         /**
          * if the number of components displaying in the main window
          * deducted by one is not equal to the index click origin
-         * it means the component newxt after the click origin index will be
-         * deleted or replaced 
+         * it means the component next after the click origin index will be
+         * deleted or be replaced 
          */
         if (n_comps_mw - 1 != i_c_origin){
             commit('pane_replace_from', { 
                 startingIndex: s_i_comp_rep, 
                 compName: component_name, 
-                numOfComps: s_i_comp_rep
+                numOfComps: s_i_comp_rep,
+                data,
+                data_index
             })
         }
     },
