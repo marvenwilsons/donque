8e73fe4a95d3ce7017861a83a39aedaa35afdd52
Marven Wilson Donque <marvenwilsons@gmail.com>
2019-09-13 23:22:47 -0400

    coverter addon for objectify

--- components/global-ui/objectify/converter.js
+++ components/global-ui/objectify/converter.js
@@ -1,5 +1,35 @@
-export default function vanilaToObjectifyObj(vanilla) {
-    let final = {}
+export default function vanilaToObjectifyObj(schema, obj) {
+    if (schema == null) {
+        return obj
+    }
 
-    return final
+
+    for (let key in schema) {
+        if (schema[key].type == 'string' || schema[key].type == 'number') {
+            if (obj){
+                if (schema[key].default != obj[key]) {
+                    schema[key].default = obj[key]
+                }
+            }            
+        } else if (schema[key].type == 'select') {
+            if(obj) {
+                if (schema[key].options[schema[key].default] != obj[key]) {
+                    schema[key].default
+
+                    const IsBoolean = (arg) => {
+                        if (arg == 'true') {
+                            return true
+                        } else if (arg == 'false') {
+                            return false
+                        } else {
+                            return arg
+                        }
+                    }
+                    schema[key].default = schema[key].options.indexOf(IsBoolean(obj[key]))
+                }
+            }
+        }
+    }
+
+    return schema
 }
