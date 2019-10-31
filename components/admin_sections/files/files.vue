6b4335cad5586358d8204f8fa11489898e643c25
Marven Wilson Donque <marvenwilsons@gmail.com>
2019-10-30 21:25:26 -0400

    Objectify usage installed

--- components/admin_sections/files/files.vue
+++ components/admin_sections/files/files.vue
@@ -1,17 +1,44 @@
 <template>
-  <div class="pad125">
-    <div class="">
+  <div class="pad125" style="width:500px">
+    <div>
+      <!-- Usage of Objectify -->
       <objectifyFlatSettings
-        @onChange="''"
+        @onChange="change"
         @onError="err"
-        operation="rw"
-        :inputData="sample_data"
-        :title="'Sample Objectify Usage'"
-        :options="{
-          borderColor: $store.state.theme.global.border_color,
-          padding: 's'
+        @onSubmit="submit"
+
+        :config="{
+          title: 'Testing Demo', // string
+          sub_title_description_text: 'This a demo form for objectify, please fill the fields below',
+          data: test_data, // object a schema
+          operation:'rw', // rw , r
+          submit_button: 'DEMO SUBMIT', // string: if supplied the button will appear
+          show_modal: modal_state
         }"
-      ></objectifyFlatSettings>
+        
+        :appearance="{
+          title_text_color: 'gray',
+          sub_title_description_text_color: 'gray',
+
+          wrap_around_border_color: 'lightgray',
+          divider_border_color:'lightgray',
+          
+          keys_bg_color: 'whitesmoke',
+          keys_text_color: 'black',
+          values_bg_color: 'whitesmoke',
+          values_text_color: 'black',
+
+          select_arrow_down_color: 'black',
+
+          button_bg_color: 'blue',
+          button_text_color: 'white',
+
+          modal_overlay_bg: 'black'         
+        }"
+      >
+        <div slot="modal" class="pad125 flex flexcenter" style="background:white;" ><pre>{{submit_data}}</pre></div>
+      </objectifyFlatSettings>
+      <!-- done usage -->
     </div>
   </div>
 </template>
@@ -21,12 +48,14 @@ export default {
   beforeCreate() {
     this.$store.commit("pane_system/set_pane_config", {
       title: "Files",
-      pane_width: "500px"
+      pane_width: "90%"
     });
   },
 
   data: () => ({
-    sample_data: {
+    submit_data: undefined,
+    modal_state: false,
+    test_data: {
       // Normal
       id: {
         type: "string",
@@ -34,34 +63,65 @@ export default {
         maxChar: 20,
         allowSpecialChars: false,
         allowWhiteSpace: false,
-        default: null
+        hoverInfo: "Element Id",
+        default: undefined
       },
       name: {
         type: "string",
         minChar: 1,
         maxChar: 100,
-        default: null
+        default: null,
+        renderCondition: {
+          controllers: ['id'],
+          method: schema => {
+            if(schema.id.default) {
+              return schema.id.default.length > 5
+            }
+          }
+        }
       },
       tabindex: {
         type: "number",
         min: 0,
         max: 999,
         step: 1,
-        default: null
+        default: null,
+        renderCondition: {
+          controllers: ['id', 'name'],
+          method: schema => {
+            if(schema.id.default) {
+              return schema.id.default.length > 2 && schema.name.default == 'test'
+            }
+          }
+        }
       },
       // 
       isAList: {
         type: "select",
         options: ["Yes", "No"],
         default: 1,
-        hoverInfo: "dq-studio global attribute: re renders element repeatedly"
+        hoverInfo: "dq-studio global attribute: re renders element repeatedly",
+        renderCondition: {
+          controllers: ['tabindex'],
+          method: schema => schema.tabindex.default == 5
+        }
       },
+      /**
+       * list origin from depends on isAList value
+       */
       "list origin from": {
         type: "select",
         options: ["models", "collections"],
         default: null,
-        hoverInfo: "dq-studio global attribute: re renders element repeatedly"
+        hoverInfo: "dq-studio global attribute: re renders element repeatedly",
+        renderCondition: {
+          controllers: ['isAList','tabindex'],
+          method: schema => schema.isAList.default == 0 && schema.tabindex.default == 5
+        }
       },
+      /**
+       * Collections list and Models depends on isAList value and list origin from value
+       */
       "collections list": {
         type: "string",
         minChar: 1,
@@ -69,8 +129,8 @@ export default {
         allowWhiteSpace: false,
         default: null,
         renderCondition: {
-          controllers: ["list origin from"],
-          method: schema => schema["list origin from"]["default"] == 1
+          controllers: ["list origin from", "isAList"],
+          method: schema => schema["list origin from"]["default"] == 1 && schema.isAList.default == 0
         }
       },
       models: {
@@ -80,13 +140,30 @@ export default {
         allowWhiteSpace: false,
         default: null,
         renderCondition: {
-          controllers: ["list origin from"],
-          method: schema => schema["list origin from"]["default"] == 0
+          controllers: ["list origin from", "isAList"],
+          method: schema => schema["list origin from"]["default"] == 0 && schema.isAList.default == 0
         }
       },
     }
   }),
 
-  methods: {}
+  methods: {
+    // triggers when error occurs
+    err(err) {
+      // console.log(err)
+    },
+
+    // triggers when change occurs
+    change(val) {
+      // console.log(val)
+    },
+
+    // triggers when sumbit button is press
+    submit(val) {
+      // console.log(val)
+      this.submit_data = val
+      this.modal_state = true
+    }
+  }
 };
 </script>
