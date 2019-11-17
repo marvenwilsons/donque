be03ed6265a4293091f32cdd05ae515150635efd
Marven Wilson Donque <marvenwilsons@gmail.com>
2019-11-16 20:24:39 -0500

    added fetch collections support

--- server/app_programs/collections/collection/fetchCollections.js
+++ server/app_programs/collections/collection/fetchCollections.js
@@ -5,6 +5,43 @@ module.exports = {
         }
     },
     async fetchCollections({dep,data}) {
-        console.log('hello world there!')
+        console.log('')
+        console.log('[Collection]'.bold.green,'----> Start')
+        console.log('[Collection]'.bold.green,'----> Executing fetchCollections function')
+
+        let cursor_collections = []
+        let collections = []
+
+        return new Promise( async (resolve,reject) => {
+            const col_cursor = await dep.db.collection('dq_collections_schema').find()
+            
+            await col_cursor.forEach((doc,err) => {
+                return cursor_collections.push(doc)
+            }).then(() => {
+                cursor_collections.map(col => {
+                    return collections.push(col[Object.keys(col)[1]])
+                })                
+                resolve({
+                    status: true,
+                    data: {
+                        msg: null,
+                        actions: [{
+                            title: null,
+                            contents: collections 
+                        }]
+                    }
+                })
+            }).catch(err => {
+                reject({
+                    status: false,
+                    data: {
+                        msg: err,
+                        actions: [{
+                            title: 'prompt_err'
+                        }]
+                    }
+                })
+            })
+        })
     }
 }
