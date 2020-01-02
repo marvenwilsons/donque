1c51e60aa3ea5935f3bfbf9ed438fe78db124460
Marven Wilson Donque <marvenwilsons@gmail.com>
2020-01-02 10:27:55 -0500

    added scafold tab for pane-settings

--- components/admin_root/dq_pane-system/pane-settings.vue
+++ components/admin_root/dq_pane-system/pane-settings.vue
@@ -1,5 +1,22 @@
 <template>
-  <div style="height:400px;">pane settings</div>
+  <div class="fullwidth fullheight-percent" style="height:400px;">
+    <div class="fullheight-percent fullwidth flex">
+      <dqTab
+        :tabs="['Raw Data', 'VDS', 'Properties']"
+        :toggleMode="'rerender'"
+        :default="0"
+        :options="{
+                  borderColor: $store.state.theme.global.border_color,
+                  activeColor: $store.state.theme.global.secondary_bg_color,
+                  activeTextColor: 'inherit',
+                }"
+      >
+        <div class="pad050 borderred fullheight-percent" slot="Raw Data">this is raw data</div>
+        <div class="pad050" slot="VDS">this is VDS</div>
+        <div slot="Properties">this is raw Properties</div>
+      </dqTab>
+    </div>
+  </div>
 </template>
 
 <script>
