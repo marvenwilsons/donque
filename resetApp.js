af8a27364cd5fb86124555fcc56f466b97bbcb9a
marven <marvenwilsons@gmail.com>
2020-03-10 18:00:03 -0400

    ubuntu migration

--- resetApp.js
+++ resetApp.js
@@ -7,16 +7,21 @@ console.log('** Reseting application')
 // const files_to_be_deleted = fs.readdirSync(path.join(__dirname, '../../Desktop/database/Data/storage.bson'))
 // console.log(files_to_be_deleted)
 
+const database_location = path.join(__dirname, '../../../Desktop/database/Data')
+
+// console.log(fs.readdirSync(database_location))
+// return
+
 const { execFile, exec, execSync, spawn } = require('child_process')
 
-execFile('mongod', ['--dbpath', '/home/marven/Desktop/database/Data', '--shutdown'], (error, stdout, stderr) => {
+execFile('mongod', ['--dbpath', database_location, '--shutdown'], (error, stdout, stderr) => {
     console.log('** Database shutdown')
 
     console.log('** Clearing database directory')
-    dqfs.rm(path.join(__dirname, '../../../Desktop/database/Data/'))
+    dqfs.rm(database_location)
 
     console.log('** Creating Data directory')
-    dqfs.mkdir(path.join(__dirname, '../../../Desktop/database/Data'))
+    dqfs.mkdir(database_location)
 
     // console.log(fs.readdirSync())
     
