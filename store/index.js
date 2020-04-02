52961485f1e5f38aa81b4b0e49c44971656760b0
marvenwilsons <marvenwilsons@gmail.com>
2020-04-01 17:12:01 -0700

    added nuxtServerInit

--- store/index.js
+++ store/index.js
@@ -3,3 +3,10 @@ export const state = () => ({
     siteName: '',
     adminPermSet: undefined
 })
+
+export const actions = {
+    nuxtServerInit (store,context) { 
+        console.log('> NuxtServerInit')
+        console.log()
+    }
+}
