c60ba6f3e664cf623bce06de60c3c2b4920db510
marvenwilsons <marvenwilsons@gmail.com>
2020-04-15 14:02:31 -0700

    added util-vies dir

---
+++ apps/util-views/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-flex></v-flex>
+</template>
+
+<script>
+import h from '@/helper'
+import Templates from '@/templates'
+export default {
+    mixins: [h],
+    props: {
+        myData: Object,
+        paneIndex: Number
+    },
+    created() {
+        this.h = this
+    },
+    data: () => ({
+
+    }),
+    methods: {
+
+    }
+}
+</script>
