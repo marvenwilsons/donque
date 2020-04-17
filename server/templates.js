824ac9e4e03c36055895b1a76143929566443cd3
marvenwilsons <marvenwilsons@gmail.com>
2020-04-17 06:52:46 -0700

    templates utils refactor in server dir

--- server/templates.js
+++ server/templates.js
@@ -1,13 +1,17 @@
 const requireFromString = require('require-from-string')
 const fs = require('fs')
+const path = require('path')
 
 const convertToNodeModuleString = (jsString) => {
   return nodeModuleString = jsString.replace('export default', 'module.exports =')
 }
 
-
-const template = convertToNodeModuleString(fs.readFileSync('../templates.js','utf8'))
-const utils = convertToNodeModuleString(fs.readFileSync('../utils.js','utf8'))
+const template = convertToNodeModuleString(
+    fs.readFileSync(path.join(__dirname,'../','templates.js'),'utf8')
+  )
+const utils = convertToNodeModuleString(
+  fs.readFileSync(path.join(__dirname,'../','utils.js'),'utf8')
+  )
 
 const combinedUtilesAndTemp = 
   utils.replace('module.exports = utils', '') + 
--- server/utils.js
+++ server/utils.js
@@ -1,10 +1,15 @@
 const requireFromString = require('require-from-string')
 const fs = require('fs')
+const fs = require('fs')
+const path = require('path')
 
 const convertToNodeModuleString = (jsString) => {
   return nodeModuleString = jsString.replace('export default', 'module.exports =')
 }
 
-const utils = convertToNodeModuleString(fs.readFileSync('../utils.js','utf8'))
+console.log('this?')
+const utils = convertToNodeModuleString(
+    fs.readFileSync(path.join(__dirname,'../utils.js'),'utf8')
+  )
 
 module.exports = requireFromString(utils)
