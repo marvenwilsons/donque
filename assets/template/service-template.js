d076a8fc4939fb35e4431855c30777b339b67177
marvenwilsons <marvenwilsons@gmail.com>
2020-04-18 11:58:39 -0700

    added service template

---
+++ assets/template/service-template.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const Templates = require(path.join(__dirname,'../../server/templates.js'))
+
+module.exports = Templates.Service({
+    name: '',
+    data: [],
+    fistPaneDefualtView: 'listify', // vue component
+    dataController: {
+        name: 'name',
+        handler: function() {
+            // execute compiled task or task here
+        },
+    },
+    views: [
+        {
+            objectKeys: [''],
+            component: '', // vue component
+            onLoad(data) {
+                
+            }
+        }
+    ],
+    config: {
+        dataSchema: {}, // define the exact sturcture of data property
+    },
+    onEmptyData() {
+
+    }
+})
