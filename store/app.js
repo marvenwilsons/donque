653478f711802504ae7b2dcec5c8f28427f34825
marvenwilsons <marvenwilsons@gmail.com>
2020-05-10 21:36:40 -0700

    refactored app store

--- store/app.js
+++ store/app.js
@@ -2,9 +2,7 @@ export const state = () => ({
     /** App Admin State */
         'app-admin-name': null,
         'app-name': null,
-        'app-admin-resources': {},
         'app-current-view': null,
-        'app-current-resource': null,
     /** ----- */
     /** App Side bar state */
         'app-admin-sidebar-items': [],
