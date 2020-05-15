b400bb9889279f9e0aca639c6d71b1725431ce67
marvenwilsons <marvenwilsons@gmail.com>
2020-05-14 20:35:49 -0700

    uniview prog 1

--- plugins/pane-views.js
+++ plugins/pane-views.js
@@ -7,6 +7,7 @@ import listify from '@/apps/util-views/components/listify/1.2.vue'
 import loading from '@/apps/util-views/components/loading/index.vue'
 import formBuilder from '@/apps/util-views/components/dqFormBuilder/index.vue'
 import raw from '@/apps/util-views/components/raw/index.vue'
+import uniview from '@/apps/util-views/components/uniview/index.vue'
 
 Vue.component("pageContent", Pages);
 Vue.component("Files", Files)
@@ -14,6 +15,7 @@ Vue.component("listify", listify)
 Vue.component("loading", loading)
 Vue.component("formBuilder",formBuilder)
 Vue.component("raw",raw)
+Vue.component("uniview", uniview)
 
 import JsonViewer from 'vue-json-viewer/ssr'
 Vue.use(JsonViewer)
