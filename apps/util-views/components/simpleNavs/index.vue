a5d44b2ca53738acacd0c629d7c61212833556dd
Marven <marvenwilsons@gmail.com>
2020-10-28 23:26:06 -0400

    added event support

--- apps/util-views/components/simpleNavs/index.vue
+++ apps/util-views/components/simpleNavs/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div style="background:white;" >
         <div v-for="(nav, index) in navs" :key="index" >
             <div class="marginleft125 marginright125 margintop125 padtop125" >
                 <strong class="" > 
@@ -14,6 +14,9 @@
                 :itemIcon="item.itemIcon"
                 :additionalContent="item.additionalContent"
                 :itemEvents="item.events"
+                :warning="item.warning"
+                :loading="ll"
+                @onEvent="bubbleEvent"
             />
         </div>
     </div>
@@ -22,95 +25,38 @@
 <script>
 import h from '@/helper'
 import Templates from '@/templates'
-import 'vue-json-viewer/style.css'
 import item from './item'
 
 export default {
     mixins: [h],
-    props:['myData','myConfig'],
+    props: ['myData','myConfig', 'paneIndex'],
     created() {
         this.h = this
     },
     data: () => ({
-        navs: [
-            {
-                name: 'Page Services',
-                items: [
-                    {
-                        name: 'Page Pool',
-                        itemIcon: 'mdi-poll',
-                        additionalContent: [
-                            {
-                                type: 'text',
-                                title: 'Description',
-                                body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ipsum aspernatur modi aliquam, expedita in distinctio enim. Officiis, ullam amet. Quia quam totam commodi aspernatur, illo facere. Porro, excepturi enim?'
-                            },
-                            {
-                                type: 'events',
-                                title: 'Other Services',
-                                body: [
-                                    {
-                                        title: 'Click and you will be directed to a dq service this is a verly long tile like super long',
-                                        eventName: 'myEvent'
-                                    },
-                                    {
-                                        title: 'Click and you will be directed to a dq service',
-                                        eventName: 'myEvent'
-                                    },
-                                    {
-                                        title: 'Click and you will be directed to a dq service this is a verly long tile like super long',
-                                        eventName: 'myEvent'
-                                    },
-                                    {
-                                        title: 'Click and you will be directed to a dq service',
-                                        eventName: 'myEvent'
-                                    }
-                                ]
-                            },
-                            {
-                                type: 'events',
-                                title: 'Tutorials',
-                                body: [
-                                    {
-                                        title: 'Click and you will be directed to a dq service this is a verly long tile like super long',
-                                        eventName: 'myEvent'
-                                    },
-                                    {
-                                        title: 'Click and you will be directed to a dq service',
-                                        eventName: 'myEvent'
-                                    },
-                                    {
-                                        title: 'Click and you will be directed to a dq service this is a verly long tile like super long',
-                                        eventName: 'myEvent'
-                                    },
-                                    {
-                                        title: 'Click and you will be directed to a dq service',
-                                        eventName: 'myEvent'
-                                    }
-                                ]
-                            },
-                        ],
-                        events: ['View', "Create", "Edit"]
-                    },
-                    {
-                        name: 'Page Layouts',
-                        itemIcon: 'mdi-border-all',
-                        additionalContent: undefined,
-                        events: ['View My Layouts','Download Page Layouts']
-                    },
-                    {
-                        name: 'Color Palletes',
-                        itemIcon: 'mdi-grain',
-                        additionalContent: undefined,
-                        events: []
-                    },
-                ]
-            },
-        ],
-        
+        navs: [],
+        ll: false
     }),
+    methods: {
+        bubbleEvent(e) {
+            this.$emit('onEvent',{
+                methods: {
+                    loading: this.loading
+                },
+                ...e
+            })
+        },
+        loading(e) {
+            this.ll = e
+        },
+        c(data,fn) {
+            const { syspane, syspanemodal, dwin} =  this.normyDep(this.paneIndex,this)
+            fn(data,syspane,syspanemodal,dwin)
+        }
+    },
     mounted() {
-        // console.log(this.myData)
+        this.navs = this.myData
+        console.log(this.myData)
     },
     components: {
         item
