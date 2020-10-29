d02e03abc439c5a6fcf6e18ec189a2a4b1627bec
Marven <marvenwilsons@gmail.com>
2020-10-29 19:47:15 -0400

    added loader animation

--- apps/util-views/components/simpleNavs/loader.vue
+++ apps/util-views/components/simpleNavs/loader.vue
@@ -1,15 +1,22 @@
 <template>
-    <div>
-        <div class="absolute fullwidth fullheight-percent flex flexcenter" 
-            style="background: #fafafa; z-index: 1; opacity: 0.7; filter: blur(10px); box-shadow: inset 0 0 2000px rgba(255, 255, 255, .5); backdrop-filter: blur(5px);" >
+    <div transition="el-zoom-in-bottom" >
+        <div id="mb" class="absolute fullwidth fullheight-percent flex flexcenter loader" 
+            style="
+                background: #fafafa; z-index: 1; 
+                opacity: 0.7; filter: 
+                blur(10px); 
+                box-shadow: inset 0 300px 3000px rgba(255, 255, 255, .9); 
+                backdrop-filter: blur(5px);
+                " 
+                >
         </div>
         <div class="absolute fullwidth fullheight-percent flex flexcenter" style="z-index: 2;" >
             <div class="flex flexcenter" >
                 <div class="relativ flex flexcenter" >
                     <strong>
-                        Fetching resources
+                        <!-- Fetching resources -->
                     </strong>
-                    <v-icon class="loader" x-small >mdi-reload</v-icon>
+                    <v-icon class="loader" small >mdi-arrow-right-bold</v-icon>
                 </div> 
                 
             </div>
@@ -19,19 +26,16 @@
 
 <style>
 .loader {
-  -webkit-animation: spin 2s linear infinite; /* Safari */
-  animation: spin 2s linear infinite;
+  -webkit-animation: spin 0.8s linear infinite; /* Safari */
+  animation: spin 0.8s linear infinite;
   border: none;
-}
-
-/* Safari */
-@-webkit-keyframes spin {
-  0% { -webkit-transform: rotate(0deg); }
-  100% { -webkit-transform: rotate(360deg); }
+  width: 160%;
 }
 
 @keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
+    0% { left: 0;}
+    25%{ left: 100%;}
+    50%{ right: 50%;}
+    100%{ right: 0;}
 }
 </style>
