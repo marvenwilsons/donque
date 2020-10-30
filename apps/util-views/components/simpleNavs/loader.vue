6f0177577c15b63dbd789b40e535acbcad34693b
Marven <marvenwilsons@gmail.com>
2020-10-29 20:08:20 -0400

    final loader

--- apps/util-views/components/simpleNavs/loader.vue
+++ apps/util-views/components/simpleNavs/loader.vue
@@ -5,6 +5,18 @@
                 background: #fafafa; z-index: 1; 
                 opacity: 0.7; filter: 
                 blur(10px); 
+                box-shadow: inset 0 300px 3500px rgba(255, 255, 255, .9); 
+                backdrop-filter: blur(5px);
+                " 
+                >
+                <!-- <div class="fullheight-percent ddt" style="width:20px; border-bottom:2px solid #389ff4; opacity: 0.5" >
+                </div> -->
+        </div>
+        <div  class="absolute fullwidth fullheight-percent flex flexcenter" 
+            style="
+                background: #fafafa; z-index: 1; 
+                opacity: 0.7; filter: 
+                blur(10px); 
                 box-shadow: inset 0 300px 3000px rgba(255, 255, 255, .9); 
                 backdrop-filter: blur(5px);
                 " 
@@ -40,15 +52,5 @@
     25%{ left: 100%;}
     50%{ right: 50%;}
     100%{ right: 0;}
-}
-
-.ddt {
-    animation: elongate 0.8s linear infinite;
-}
-@keyframes elongate {
-    0% { width: 0;}
-    25%{ width: 100%;}
-    50%{ width: 50%;}
-    100%{ width: 0;}
 }
 </style>
