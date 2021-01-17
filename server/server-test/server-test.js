02d99fac2fd3f307270a7648f6b20c671dc295db
Marven <marvenwilsons@gmail.com>
2021-01-17 10:53:32 -0500

    added documentation for test-runner

--- server/server-test/server-test.js
+++ server/server-test/server-test.js
@@ -32,6 +32,9 @@ const getTestData = (of) => {
   return extractDataSet
 }
 
+/**
+ * Display JSON object to terminal
+ */
 const displayJSON = (title,val) => {
   console.log(`
       ${title}
@@ -41,8 +44,9 @@ const displayJSON = (title,val) => {
   })
 }
 
+/** Start The Test */
 (async (getTestData,displayJSON) => {
-    // 1
+    /** Prompt to select Data Set */
     const promptResponse = await prompts({
         type: 'select',
         name: 'value',
@@ -55,10 +59,23 @@ const displayJSON = (title,val) => {
     displayJSON('Selected Test Data',promptResponse.value.data)
     displayJSON('Test Config', promptResponse.value.testConfig)
     
+    /**
+     * Extract Test Config
+     * filepath_to_test:  is the physical location of the .js file
+     * method_to_test:    the method to execute when the test runner starts, only works when the file exports a collection of methods 
+     * restart_test_on_file_change: Boolean, you know what it means
+     * dir_to_watch: physical location of the directory
+     */
     const { filepath_to_test, method_to_test, restart_test_on_file_change, dir_to_watch  } =  promptResponse.value.testConfig
     
+    /**
+     * Locating the file to be tested
+     */
     const getFileToTest = require(path.join(__dirname, `../../${filepath_to_test}`))
-
+    
+    /**
+     * Get the names of the arguments in the function that is being tested
+     */
     var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
     var ARGUMENT_NAMES = /([^\s,]+)/g;
     function getParamNames(func) {
@@ -69,12 +86,26 @@ const displayJSON = (title,val) => {
       return result;
     }
 
+    /**
+     * This block is for terminal printing purposes
+     * it prints the parameters of the function as a JSON
+     */
     if(method_to_test) {
       displayJSON('Params', getParamNames(getFileToTest[method_to_test]))
     } else {
       displayJSON('Params', getParamNames(getFileToTest))
     }
 
+    /**
+     * The ExecFunc Function is responsilbe for executing the function that is being tested with
+     * the selected data set, the selected data set feeds into the middleware of the function located
+     * on the test directory specified on script run --> 'npm run serverTest <dirName>'
+     * 
+     * the middleware is responsible for executing the necessary scripts of task before before running the 
+     * function that is being tested
+     * 
+     * the middleware serves as the preparation routine for the function that is being tested
+     */
     const execFunc = () => {
       if(method_to_test) {
         /** when test is file exports an object */
@@ -113,8 +144,14 @@ const displayJSON = (title,val) => {
       }
     }
 
+    /**
+     * Execute test
+     */
     execFunc()
 
+    /**
+     * File watcher
+     */
     if(restart_test_on_file_change === true) {
       const w = watcher(path.join(__dirname,`../${dir_to_watch}`))
       w.on('fileChanged', () => {
