f99fd6aa50e381351c03c66085b70a94cf6e9112
Marven <marvenwilsons@gmail.com>
2021-01-19 09:26:11 -0500

    refactor utils

--- server/helpers/utils.js
+++ server/helpers/utils.js
@@ -1,15 +1,14 @@
 const requireFromString = require('require-from-string')
 const fs = require('fs')
-const fs = require('fs')
 const path = require('path')
 
 const convertToNodeModuleString = (jsString) => {
+  /** converting js modules to node modules */
   return nodeModuleString = jsString.replace('export default', 'module.exports =')
 }
 
-console.log('this?')
 const utils = convertToNodeModuleString(
-    fs.readFileSync(path.join(__dirname,'../utils.js'),'utf8')
+    fs.readFileSync(path.join(__dirname,'../../utils.js' /** refers to root dir */),'utf8')
   )
 
-// module.exports = requireFromString(utils)
+module.exports = requireFromString(utils)
